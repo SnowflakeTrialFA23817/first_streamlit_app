ac8d8068c989d71f03d707a164a290561a2f88f7
SnowflakeTrialFA23817 <107418515+SnowflakeTrialFA23817@users.noreply.github.com>
2022-06-13 16:40:04 +0100

    Update streamlit_app.py
    Changed title
    Limited dataframe to show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title('My Parents Healthy New Diner')
+streamlit.title('My Mums Healthy New Diner')
 
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -14,7 +14,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Adding a picklist for the fruits they want in the smoothie
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Displaying the fruit list in a table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)

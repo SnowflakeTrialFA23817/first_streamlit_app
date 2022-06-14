5cd84c5c866a9fe534aba2c4d80cee52be9ae657
SnowflakeTrialFA23817 <107418515+SnowflakeTrialFA23817@users.noreply.github.com>
2022-06-14 10:36:38 +0100

    Update streamlit_app.py
    Add user input to determine what fruit to query at Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruityvice API response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
+#User input to determine what fruit to get advice about
+fruity_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 streamlit.header('Fruityvice Fruit Advice!')
 

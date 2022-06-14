f1dc0f3b0ba5940f8c3759e0d51a15ce6af4f85e
SnowflakeTrialFA23817 <107418515+SnowflakeTrialFA23817@users.noreply.github.com>
2022-06-14 12:15:15 +0100

    Update streamlit_app.py
    rearrange to keep fruityvice code together and snowflake code together

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 #take the response json and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output the normalized json on screen as a table
+streamlit.dataframe(fruityvice_normalized)
 
 #Snowflake connection information and display
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -41,6 +43,3 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-
-#output the normalized json on screen as a table
-streamlit.dataframe(fruityvice_normalized)

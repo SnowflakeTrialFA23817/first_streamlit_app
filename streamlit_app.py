59355bc38528f5e7a0dbf0d9c8326e28e41135d7
SnowflakeTrialFA23817 <107418515+SnowflakeTrialFA23817@users.noreply.github.com>
2022-06-14 10:38:58 +0100

    Update streamlit_app.py
    Amended typo in variable fruity_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ import requests
 
 #User input to determine what fruit to get advice about
 fruity_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered',fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+streamlit.write('The user entered',fruity_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruity_choice)
 
 streamlit.header('Fruityvice Fruit Advice!')
 

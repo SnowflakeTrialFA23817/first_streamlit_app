2430d44a0d442544237dc9f1517d3d66a640b88b
SnowflakeTrialFA23817 <107418515+SnowflakeTrialFA23817@users.noreply.github.com>
2022-06-14 13:29:18 +0100

    Update streamlit_app.py
    add missing colon to if statement re fruity_choice variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
   #User input to determine what fruit to get advice about
   fruity_choice = streamlit.text_input('What fruit would you like information about?')
   
-  if not fruit_choice
+  if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruity_choice)
